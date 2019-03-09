0a335bda32e5ae3ebfc704a936e096fe10bcaef0
Tim Carman <tpcarman@gmail.com>
2019-03-09 23:11:58 +1100

    Updates for page orientation settings in default style

--- AsBuiltReport.VMware.vSphere.Style.ps1
+++ AsBuiltReport.VMware.vSphere.Style.ps1
@@ -1,6 +1,9 @@
-#region VMware Document Style
+# VMware Default Document Style
+
+# Configure document options
 DocumentOption -EnableSectionNumbering -PageSize A4 -DefaultFont Arial -MarginLeftAndRight 71 -MarginTopAndBottom 71 -Orientation $Orientation
 
+# Configure Heading and Font Styles
 Style -Name 'Title' -Size 24 -Color '002538' -Align Center
 Style -Name 'Title 2' -Size 18 -Color '007CBB' -Align Center
 Style -Name 'Title 3' -Size 12 -Color '007CBB' -Align Left
@@ -20,32 +23,37 @@ Style -Name 'Warning' -Size 10 -BackgroundColor 'FFE860'
 Style -Name 'Info' -Size 10 -BackgroundColor 'A6D8E7'
 Style -Name 'OK' -Size 10 -BackgroundColor 'AADB1E'
 
+# Configure Table Styles
 TableStyle -Id 'TableDefault' -HeaderStyle 'TableDefaultHeading' -RowStyle 'TableDefaultRow' -AlternateRowStyle 'TableDefaultAltRow' -BorderColor '002538' -Align Left -BorderWidth 0.5 -Default
 TableStyle -Id 'Borderless' -BorderWidth 0
 
-# VMware Cover Page
-BlankLine -Count 11
-Paragraph -Style Title $Global:ReportConfig.Report.Name
-if ($Global:AsBuiltConfig.Company.FullName) {
-    Paragraph -Style Title2 $Global:AsBuiltConfig.Company.FullName
-    BlankLine -Count 27
-    Table -Name 'Cover Page' -List -Style Borderless -Width 0 -Hashtable ([Ordered] @{
-            'Author:' = $Global:AsBuiltConfig.Report.Author
-            'Date:' = Get-Date -Format 'dd MMMM yyyy'
-            'Version:' = $Global:ReportConfig.Report.Version
-        })
-    PageBreak
+# VMware Cover Page Layout
+# Set position of report titles and information based on page orientation
+if ($Orientation -eq 'Portrait') {
+    BlankLine -Count 11
+    $LineCount = 30
 } else {
-    BlankLine -Count 28
-    Table -Name 'Cover Page' -List -Style Borderless -Width 0 -Hashtable ([Ordered] @{
-            'Author:' = $Global:AsBuiltConfig.Report.Author
-            'Date:' = Get-Date -Format 'dd MMMM yyyy'
-            'Version:' = $Global:ReportConfig.Report.Version
-        })
-    PageBreak
+    BlankLine -Count 7
+    $LineCount = 20
 }
 
-# Table of Contents
+# Add Report Name
+Paragraph -Style Title $ReportConfig.Report.Name
+
+if ($AsBuiltConfig.Company.FullName) {
+    # Add Company Name if specified
+    Paragraph -Style Title2 $AsBuiltConfig.Company.FullName
+    BlankLine -Count $LineCount
+} else {
+    BlankLine -Count ($LineCount + 1)
+}
+Table -Name 'Cover Page' -List -Style Borderless -Width 0 -Hashtable ([Ordered] @{
+        'Author:' = $AsBuiltConfig.Report.Author
+        'Date:' = Get-Date -Format 'dd MMMM yyyy'
+        'Version:' = $ReportConfig.Report.Version
+    })
+PageBreak
+
+# Add Table of Contents
 TOC -Name 'Table of Contents'
 PageBreak
-#endregion VMware Document Style
